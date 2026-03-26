27d9b2fcc35c06c54c5ce09f9d45206088d8f7e3
praveenbesetti <praveenbesetti0@gmail.com>
2026-03-26 10:24:31 +0530

    url update

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-// export const API_BASE = "https://web-app-backend.duckdns.org/api";
-export const API_BASE="http://34.31.237.54/api"
+export const API_BASE = "https://myfarmapp.duckdns.org/api";
+// export const API_BASE="http://34.31.237.54/api"
